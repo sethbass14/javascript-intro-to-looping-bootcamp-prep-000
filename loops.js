a55076fba22d5ebbf611356c3fe40c8c595090f9
Seth Barden <sethbarden@gmail.com>
2017-09-06 20:28:50 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -15,14 +15,3 @@ function whileLoop(num) {
   }
   return 'done'
 }
-
-function maybeTrue() {
-  return Math.random() >= 0.5 // Returns a random number between 0 (inclusive) and 1 (exclusive)
-}
-
-function doWhileLoop(array) {
-  do {
-    array.pop
-  } while array.length > 0 && maybeTrue();
-  return array
-}
