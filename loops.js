5789e8d9d0f8893a3171a46ea50d6bfda4de3fa4
Seth Barden <sethbarden@gmail.com>
2017-09-06 20:38:01 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -23,7 +23,7 @@ function maybeTrue() {
 function doWhileLoop(array) {
   do {
     array.pop
-  } while (array.length > 0 && maybeTrue()) {
+  } while (array.length > 0 && maybeTrue()); {
     array.pop
   }
   return array
